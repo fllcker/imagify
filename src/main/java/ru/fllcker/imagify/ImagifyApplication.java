d166c4bf38db4cacb86da4550bcdff0ee3a42ee8
fllcker <jxwide@gmail.com>
2023-02-03 17:19:15 +0300

    init

---
+++ src/main/java/ru/fllcker/imagify/ImagifyApplication.java
@@ -0,0 +1,13 @@
+package ru.fllcker.imagify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagifyApplication.class, args);
+    }
+
+}
